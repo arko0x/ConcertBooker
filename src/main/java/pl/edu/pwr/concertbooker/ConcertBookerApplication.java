0b66410636e0f53c1c1a601b5fb73ba035db4769
arko0x <arkadiusz88084@gmail.com>
2022-05-13 17:20:19 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pwr/concertbooker/ConcertBookerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.concertbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pwr/concertbooker/ConcertBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.concertbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertBookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
